d39a6bb6771207215a6f49bb8574cf0848f37e48
vekshan <vekshan@gmail.com>
2021-03-21 04:01:01 -0400

    [NEW] Arrange tables in dashboard and save queries used

--- olap_queries.sql
+++ olap_queries.sql
@@ -94,3 +94,97 @@ WINDOW W AS
 ORDER BY d.full_date
 RANGE BETWEEN  '15 days' PRECEDING
 AND '15 days' FOLLOWING)
+
+
+/*Tableau */
+
+
+/*  */
+SELECT
+s.title AS special_measure, 
+s.description,
+s.start_date,
+s.end_date,
+SUM(f.resolved ::INT) AS total_cases_resolved,
+SUM(f.unresolved ::INT) AS total_cases_unresolved,
+SUM(f.fatal ::INT) AS total_cases_fatal,
+COUNT(*) AS total_cases
+FROM 
+covid19_tracking_fact_table f,
+special_measures_dimension s
+WHERE
+f.special_measures_surrogate_key = s.special_measures_surrogate_key 
+GROUP BY s.special_measures_surrogate_key
+
+/*  */
+SELECT
+s.title AS special_measure, 
+s.description,
+s.start_date,
+s.end_date,
+SUM(f.resolved ::INT) AS total_cases_resolved,
+SUM(f.unresolved ::INT) AS total_cases_unresolved,
+SUM(f.fatal ::INT) AS total_cases_fatal,
+COUNT(*) AS total_cases
+FROM 
+covid19_tracking_fact_table f,
+special_measures_dimension s
+WHERE
+f.special_measures_surrogate_key = s.special_measures_surrogate_key 
+GROUP BY s.special_measures_surrogate_key
+
+/*  */
+SELECT
+d.full_date,
+TO_CHAR(TO_DATE(d.month::text, 'MM'), 'Month') AS onset_month,
+f.total_cases,
+AVG(f.total_cases) OVER W AS moving_average
+FROM 
+(
+ SELECT onset_date_surrogate_key, phu_location_surrogate_key, COUNT(*) as total_cases 
+ FROM covid19_tracking_fact_table 
+ GROUP BY (onset_date_surrogate_key, phu_location_surrogate_key)
+) AS f,
+phu_location_dimension l,
+onset_date_dimension d
+WHERE
+f.phu_location_surrogate_key = l.phu_location_surrogate_key
+AND
+f.onset_date_surrogate_key = d.date_surrogate_key
+GROUP BY d.month, f.total_cases, d.full_date
+WINDOW W AS 
+(ORDER BY d.full_date
+RANGE BETWEEN  '7 days' PRECEDING
+AND '7 days' FOLLOWING)
+
+
+/* To be considered */
+
+SELECT
+l.phu_name,
+d.full_date,
+f.total_cases,
+f.acquisition_group,
+AVG(f.total_cases) OVER W AS moving_average
+FROM 
+(
+	SELECT onset_date_surrogate_key, phu_location_surrogate_key, COUNT(*) as total_cases, acquisition_group 
+	FROM covid19_tracking_fact_table 
+	INNER JOIN patient_dimension 
+	ON covid19_tracking_fact_table.patient_surrogate_key = patient_dimension.patient_surrogate_key
+	GROUP BY (acquisition_group, onset_date_surrogate_key, phu_location_surrogate_key)
+) AS f
+INNER JOIN 
+phu_location_dimension l
+ON
+f.phu_location_surrogate_key = l.phu_location_surrogate_key
+INNER JOIN 
+onset_date_dimension d
+ON
+f.onset_date_surrogate_key = d.date_surrogate_key
+GROUP BY
+l.phu_name, d.full_date, f.total_cases, f.acquisition_group
+WINDOW W AS 
+(ORDER BY d.full_date
+RANGE BETWEEN  '7 days' PRECEDING
+AND '7 days' FOLLOWING)
