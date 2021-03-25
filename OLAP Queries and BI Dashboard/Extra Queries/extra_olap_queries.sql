3a43c6afe26057e02111c3fdd1172fe136abf639
vekshan <36571428+vekshan@users.noreply.github.com>
2021-03-25 15:18:05 -0400

    [Fix] Fixed Dashboard Layout, Graphs/Charts Naming & Labels, and Moved Queries into Separate Files (#16)
    * [NEW] Change dashboard layout
    
    * [Fix] [WIP] Ordered queries into correct files
    
    * [Fix] Updated Graph Axis Names & Put Screenshot(s) into their own Folder
    
    * [New] Added Screenshots for Dashboard Pages & Fixed Dashboard 2 Layout
    
    * [FIX] Changed alias to full_name
    
    * [FIX] ADD Final Dashboard according to new requirements
    
    * [FIX] Changed SubHeadings to meet axis
    
    * [Fix] Fixed spelling and formatting of both query files
    
    * [Fix] Fixed spelling in OLAP Query file
    
    * [Fix] Fixed labels for graph on Dashboard
    
    * [FIX] Fix general formatting and layout
    
    * [FIX] Fixed one rollup comment in extra queries
    
    * [FIX] Title for Roll Up graph
    
    Co-authored-by: Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>

---
+++ OLAP Queries and BI Dashboard/Extra Queries/extra_olap_queries.sql
@@ -0,0 +1,90 @@
+/*Extra Queries made for Parts 1, 2 & 3*/
+
+/*Drill Down Query (Positive Cases & October 2020)*/
+SELECT D.full_date, COALESCE(sum(CASE WHEN F.unresolved THEN 1 ELSE 0 END), 0) as total_unresolved_cases
+FROM covid19_tracking_fact_table AS F, reported_date_dimension AS D
+WHERE F.reported_date_surrogate_key = D.date_surrogate_key AND (D.full_date >= '2020-10-01' AND D.full_date <= '2020-10-31')
+GROUP BY (D.full_date)
+ORDER BY D.full_date
+
+/*Drill Down Query (Resolved Cases & Week 41 in 2020)*/
+SELECT D.Day, D.Week_in_Year, D.Month, D.Year, SUM(F.resolved::INT) AS total_resolve_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key AND week_in_year = 41
+GROUP BY (D.year, D.month, D.Week_in_Year, D.day)
+ORDER BY D.year, D.month, D.Week_in_Year, D.day
+
+/*Roll Up Query (Total Fatal Cases rollup to City)*/
+SELECT L.city, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, phu_location_dimension AS L
+WHERE F.phu_location_surrogate_key = L.phu_location_surrogate_key
+GROUP BY ROLLUP(L.city)
+ORDER BY L.city
+
+/*Roll Up Query (Total Fatal Cases rollup to Region and City by Gender and Age Group)*/
+SELECT L.city, I.age_group, I.gender, (CASE WHEN L.city = 'Ottawa' THEN 'Ottawa' ELSE 'Toronto' END) AS region, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, phu_location_dimension AS L, patient_dimension as I
+WHERE F.phu_location_surrogate_key = L.phu_location_surrogate_key AND F.patient_surrogate_key = I.patient_surrogate_key
+GROUP BY I.age_group, I.gender, ROLLUP(region, L.city)
+ORDER BY L.city
+
+/*Dice Query (Total Fatal Cases for Sub Region and Precipitation)*/
+SELECT M.subregion, M.parks, M.transit_stations, W.precipitation, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key 
+AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.precipitation > 0
+GROUP BY (M.subregion, M.parks, M.transit_stations, W.precipitation)
+
+/*Dice Query (Total Cases for Sub Region and Sunny Days)*/
+SELECT M.subregion, M.parks, M.transit_stations, W.daily_high_temperature, COUNT(*) AS total_cases
+FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key 
+AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.daily_low_temperature >= 17
+GROUP BY (M.subregion, M.parks, M.transit_stations, W.daily_high_temperature)
+
+/*Combined OLAP Operations Query (Mobility Levels Contrast Between Ottawa and Toronto)*/
+SELECT D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential, COUNT(*) AS total_cases
+FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, onset_date_dimension AS D
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.onset_date_surrogate_key = D.date_surrogate_key AND
+M.subregion in ('Ottawa Division', 'Toronto Division')
+GROUP BY (D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential)
+ORDER BY D.full_date
+
+/*Iceberg Query (Top-N for Total Resolved Cases on a Date)*/
+SELECT D.full_date, SUM(F.resolved::INT) AS total_resolved_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key
+GROUP BY D.full_date
+ORDER BY total_resolved_cases DESC
+LIMIT 5
+
+/*Iceberg Query (Top-N for Total Fatal Cases on a Date)*/
+SELECT D.full_date, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key
+GROUP BY D.full_date
+ORDER BY total_fatal_cases DESC
+LIMIT 5
+
+/*Iceberg Query (Bottom-N for Total Resolved Cases on a Date)*/
+SELECT D.full_date, SUM(F.resolved::INT) AS total_resolved_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key
+GROUP BY D.full_date
+ORDER BY total_resolved_cases ASC
+LIMIT 5
+
+/*Iceberg Query (Bottom-N for Total Resolved Cases on a Date)*/
+SELECT D.full_date, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key
+GROUP BY D.full_date
+ORDER BY total_fatal_cases ASC
+LIMIT 5
+
+/*Windowing Query (Total Resolved Cases rank within the Weeks of the Four Selected Monthes)*/
+SELECT D.quarter, TO_CHAR(TO_DATE(D.month::text, 'MM'), 'Month') AS Month, D.week_in_year, SUM(F.resolved ::INT) AS total_cases_resolved, 
+RANK() OVER (PARTITION BY D.month ORDER BY SUM(F.resolved ::INT) DESC)
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key
+GROUP BY (D.quarter, D.month, D.week_in_year)
--- OLAP Queries and BI Dashboard/olap_queries.sql
+++ OLAP Queries and BI Dashboard/olap_queries.sql
@@ -1,7 +1,32 @@
--- b(ii), c(i), d(ii)
+/*Part 1. Standard OLAP Operations - 9 Queries*/
 
-/*  */
+/*a. Drill Down and Roll Up - 2 Queries*/
 
+/*Drill Down Query (Positive Cases & October 2020)*/
+SELECT D.year, D.week_in_year, D.month, D.day, SUM(F.unresolved::INT) AS total_unresolved_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key AND D.month = 10 AND D.year = 2020
+GROUP BY (D.year, D.week_in_year, D.month, D.day)
+ORDER BY D.year, D.week_in_year, D.month, D.day
+
+/*Roll Up Query (Total Fatal Cases rollup to Region and City)*/
+SELECT L.city, (CASE WHEN L.city = 'Ottawa' THEN 'Ottawa' ELSE 'Toronto' END) AS region, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, phu_location_dimension AS L
+WHERE F.phu_location_surrogate_key = L.phu_location_surrogate_key
+GROUP BY ROLLUP(region, L.city)
+ORDER BY L.city
+
+/*b. Slice - 2 Queries*/
+
+/*Slice Query (Total Cases in York Region Public Health Services)*/
+SELECT L.phu_name, SUM(F.resolved ::INT) AS total_cases_resolved, SUM(F.unresolved ::INT) AS total_cases_unresolved, SUM(F.fatal ::INT) AS total_cases_fatal, COUNT(P.patient_surrogate_key) AS total_cases
+FROM covid19_tracking_fact_table AS F 
+INNER JOIN patient_dimension P ON F.patient_surrogate_key = P.patient_surrogate_key
+INNER JOIN phu_location_dimension L ON F.phu_location_surrogate_key = L.phu_location_surrogate_key
+WHERE phu_name = 'York Region Public Health Services'
+GROUP BY L.phu_name
+
+/*Slice Query (Total Cases during Stage 3)*/
 SELECT 
 l.phu_name,
 SUM(f.resolved ::INT) AS total_cases_resolved,
@@ -26,8 +51,16 @@ s.title = 'Stage 3 Modified'
 GROUP BY
 l.phu_name
 
-/*  */
+/*c. Dice - 2 Queries*/
 
+/* Dice Query (Total Unresolved Cases for Sub Region and Precipitation)*/
+SELECT M.subregion, M.parks, M.transit_stations, W.precipitation, SUM(F.unresolved::INT) AS total_unresolved_cases
+FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key 
+AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.precipitation > 0
+GROUP BY (M.subregion, M.parks, M.transit_stations, W.precipitation)
+
+/* Dice Query (Total Fatal Cases for PHU City Locations of Missauga and Ottawa in July and August)*/
 SELECT
 l.phu_name,
 COUNT(p.patient_surrogate_key) AS total_cases_fatal
@@ -53,89 +86,68 @@ d.month IN (7,8)
 GROUP BY
 l.phu_name
 
-/*  */
+/*d. Combining OLAP Operations - 3 Queries*/
 
-SELECT
-s.title AS special_measure, 
-s.description,
-s.start_date,
-s.end_date,
-SUM(f.resolved ::INT) AS total_cases_resolved,
-SUM(f.unresolved ::INT) AS total_cases_unresolved,
-SUM(f.fatal ::INT) AS total_cases_fatal,
-COUNT(*) AS total_cases
-FROM 
-covid19_tracking_fact_table f,
-special_measures_dimension s
+/*Combined OLAP Operations Query (Outcomes and Weather Conditions for Cold Days with Precipitation)*/
+SELECT M.subregion, W.daily_low_temperature, W.precipitation, SUM(F.resolved::INT) AS total_resolved_cases, 
+SUM(F.unresolved::INT) AS total_unresolved_cases, SUM(F.fatal::INT) AS total_fatal_cases
+FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key
+AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.daily_low_temperature <= 4 AND W.precipitation >= 0
+GROUP BY (M.subregion, W.daily_low_temperature, W.precipitation)
+
+/*Combined OLAP Operations Query (Contrast Weather Conditions, Special Measure and Total Cases)*/
+SELECT M.subregion, S.title, W.daily_high_temperature, W.daily_low_temperature, W.precipitation, AVG(F.total_cases) AS average_total_cases
+FROM (
+ SELECT onset_date_surrogate_key, mobility_surrogate_key, weather_surrogate_key, special_measures_surrogate_key, COUNT(*) as total_cases 
+ FROM covid19_tracking_fact_table 
+ GROUP BY (onset_date_surrogate_key, mobility_surrogate_key, weather_surrogate_key, special_measures_surrogate_key)
+) AS F, mobility_dimension AS M, weather_dimension as W, special_measures_dimension as S
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key AND F.special_measures_surrogate_key = S.special_measures_surrogate_key 
+AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.daily_low_temperature >= 17 AND W.precipitation <= 2.5 AND S.title = 'Stage 3'
+GROUP BY (M.subregion, S.title, W.daily_high_temperature, W.daily_low_temperature, W.precipitation)
+
+/*Combined OLAP Operations Query (Contrasting Mobility Levels in Ottawa and Toronto)*/
+SELECT D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential, COUNT(*) AS total_cases
+FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, onset_date_dimension AS D
+WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.onset_date_surrogate_key = D.date_surrogate_key AND
+M.subregion in ('Ottawa Division', 'Toronto Division')
+GROUP BY (D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential)
+ORDER BY D.full_date
+
+/*Part 2. Explorative Operations - 3 Queries*/
+
+/*a. Iceberg Query*/
+
+/*Iceberg Query (Top-N for Total Cases on a Date)*/
+SELECT D.full_date, COUNT(*) AS total_cases
+FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
+WHERE F.onset_date_surrogate_key = D.date_surrogate_key
+GROUP BY D.week
+ORDER BY total_cases DESC
+LIMIT 5
+
+/*b. Windowing Query*/
+
+/*Windowing Query (Total Cases partitioned by age_group, ranked by total by acquisition group)*/
+Select p.acquisition_group, p.age_group, COUNT(*) as total_cases, 
+RANK () OVER ( 
+		PARTITION BY p.age_group
+		ORDER BY COUNT(*) DESC
+	) as rank
+FROM
+covid19_tracking_fact_table f, 
+patient_dimension p 
 WHERE
-f.special_measures_surrogate_key = s.special_measures_surrogate_key 
-GROUP BY s.special_measures_surrogate_key
+f.patient_surrogate_key = p.patient_surrogate_key
+AND acquisition_group != 'MISSING INFORMATION'
+AND age_group != 'UNKNOWN'
+GROUP BY
+(p.age_group, p.acquisition_group)
 
-/*  */
+/*c. Using the Window Clause Query*/
 
-SELECT
-d.full_date,
-l.city,
-TO_CHAR(TO_DATE(d.month::text, 'MM'), 'Month') AS onset_month,
-f.number_resolved,
-AVG(f.number_resolved) OVER W AS moving_avg_resolved
-FROM 
-(SELECT onset_date_surrogate_key, phu_location_surrogate_key, SUM(resolved::INT) as number_resolved FROM covid19_tracking_fact_table 
-GROUP BY (onset_date_surrogate_key, phu_location_surrogate_key, resolved)) AS f,
-phu_location_dimension l,
-onset_date_dimension d
-WHERE
-f.phu_location_surrogate_key = l.phu_location_surrogate_key
-AND
-f.onset_date_surrogate_key = d.date_surrogate_key
-AND l.city = 'Ottawa'
-GROUP BY d.month, l.city, f.number_resolved, d.full_date
-WINDOW W AS 
-(PARTITION BY l.city
-ORDER BY d.full_date
-RANGE BETWEEN  '15 days' PRECEDING
-AND '15 days' FOLLOWING)
-
-/*Tableau */
-
-/*  */
-
-SELECT
-s.title AS special_measure, 
-s.description,
-s.start_date,
-s.end_date,
-SUM(f.resolved ::INT) AS total_cases_resolved,
-SUM(f.unresolved ::INT) AS total_cases_unresolved,
-SUM(f.fatal ::INT) AS total_cases_fatal,
-COUNT(*) AS total_cases
-FROM 
-covid19_tracking_fact_table f,
-special_measures_dimension s
-WHERE
-f.special_measures_surrogate_key = s.special_measures_surrogate_key 
-GROUP BY s.special_measures_surrogate_key
-
-/*  */
-
-SELECT
-s.title AS special_measure, 
-s.description,
-s.start_date,
-s.end_date,
-SUM(f.resolved ::INT) AS total_cases_resolved,
-SUM(f.unresolved ::INT) AS total_cases_unresolved,
-SUM(f.fatal ::INT) AS total_cases_fatal,
-COUNT(*) AS total_cases
-FROM 
-covid19_tracking_fact_table f,
-special_measures_dimension s
-WHERE
-f.special_measures_surrogate_key = s.special_measures_surrogate_key 
-GROUP BY s.special_measures_surrogate_key
-
-/*  */
-
+/*Window Clause Query (Number of Resolved Cases on a Particular Date compared to a 14 Day Moving Average)*/
 SELECT
 d.full_date,
 TO_CHAR(TO_DATE(d.month::text, 'MM'), 'Month') AS onset_month,
@@ -158,233 +170,3 @@ WINDOW W AS
 (ORDER BY d.full_date
 RANGE BETWEEN  '7 days' PRECEDING
 AND '7 days' FOLLOWING)
-
-/*  */
-
-SELECT
-l.phu_name,
-d.season,
-d.full_date,
-p.acquisition_group,
-COUNT(p.patient_surrogate_key) AS total_cases
-FROM 
-covid19_tracking_fact_table f 
-INNER JOIN 
-patient_dimension p
-ON f.patient_surrogate_key = p.patient_surrogate_key
-INNER JOIN 
-phu_location_dimension l
-ON
-f.phu_location_surrogate_key = l.phu_location_surrogate_key
-INNER JOIN 
-onset_date_dimension d
-ON
-f.onset_date_surrogate_key = d.date_surrogate_key
-WHERE
-p.acquisition_group in ('CS', 'CC')
-GROUP BY
-l.phu_name, d.full_date, d.season, p.acquisition_group
-ORDER BY d.full_date
-
-/* To be considered */
-
-SELECT
-l.phu_name,
-d.full_date,
-f.total_cases,
-f.acquisition_group,
-AVG(f.total_cases) OVER W AS moving_average
-FROM 
-(
-	SELECT onset_date_surrogate_key, phu_location_surrogate_key, COUNT(*) as total_cases, acquisition_group 
-	FROM covid19_tracking_fact_table 
-	INNER JOIN patient_dimension 
-	ON covid19_tracking_fact_table.patient_surrogate_key = patient_dimension.patient_surrogate_key
-	GROUP BY (acquisition_group, onset_date_surrogate_key, phu_location_surrogate_key)
-) AS f
-INNER JOIN 
-phu_location_dimension l
-ON
-f.phu_location_surrogate_key = l.phu_location_surrogate_key
-INNER JOIN 
-onset_date_dimension d
-ON
-f.onset_date_surrogate_key = d.date_surrogate_key
-GROUP BY
-l.phu_name, d.full_date, f.total_cases, f.acquisition_group
-WINDOW W AS 
-(ORDER BY d.full_date
-RANGE BETWEEN  '7 days' PRECEDING
-AND '7 days' FOLLOWING)
-
-/* Sukhu's Queries */
-
-/*Drill Down Query (October 2020)*/
-SELECT D.year, D.week_in_year, D.month, D.day, SUM(F.unresolved::INT) AS total_unresolved_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key AND D.month = 10 AND D.year = 2020
-GROUP BY (D.year, D.week_in_year, D.month, D.day)
-ORDER BY D.year, D.week_in_year, D.month, D.day
-
-/*Drill Down Query (October 2020)*/
-SELECT D.full_date, COALESCE(sum(CASE WHEN F.unresolved THEN 1 ELSE 0 END), 0) as total_unresolved_cases
-FROM covid19_tracking_fact_table AS F, reported_date_dimension AS D
-WHERE F.reported_date_surrogate_key = D.date_surrogate_key AND (D.full_date >= '2020-10-01' AND D.full_date <= '2020-10-31')
-GROUP BY (D.full_date)
-ORDER BY D.full_date
-
-/*Roll Up Query (Total Fatal Cases rollup to City)*/
-SELECT L.city, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, phu_location_dimension AS L
-WHERE F.phu_location_surrogate_key = L.phu_location_surrogate_key
-GROUP BY ROLLUP(L.city)
-ORDER BY L.city
-
-/*Roll Up Query (Total Fatal Cases rollup to Region and City)*/
-SELECT L.city, (CASE WHEN L.city = 'Ottawa' THEN 'Ottawa' ELSE 'Toronto' END) AS region, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, phu_location_dimension AS L
-WHERE F.phu_location_surrogate_key = L.phu_location_surrogate_key
-GROUP BY ROLLUP(region, L.city)
-ORDER BY L.city
-
-/*Roll Up Query (Total Fatal Cases rollup to Age Group, Gender, Region and City)*/
-SELECT L.city, I.age_group, I.gender, (CASE WHEN L.city = 'Ottawa' THEN 'Ottawa' ELSE 'Toronto' END) AS region, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, phu_location_dimension AS L, patient_dimension as I
-WHERE F.phu_location_surrogate_key = L.phu_location_surrogate_key AND F.patient_surrogate_key = I.patient_surrogate_key
-GROUP BY I.age_group, I.gender, ROLLUP(region, L.city)
-ORDER BY L.city
-
-/*Dice Query (Total Fatal Cases for Sub Region and Precipitation)*/
-SELECT M.subregion, M.parks, M.transit_stations, W.precipitation, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key 
-AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.precipitation > 0
-GROUP BY (M.subregion, M.parks, M.transit_stations, W.precipitation)
-
-/* Dice Query (Total Unresolved Cases for Sub Region and Precipitation)*/
-SELECT M.subregion, M.parks, M.transit_stations, W.precipitation, SUM(F.unresolved::INT) AS total_unresolved_cases
-FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key 
-AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.precipitation > 0
-GROUP BY (M.subregion, M.parks, M.transit_stations, W.precipitation)
-
-/*Dice Query (Total Cases for Sub Region and Sunny Days)*/
-SELECT M.subregion, M.parks, M.transit_stations, W.daily_high_temperature, COUNT(*) AS total_cases
-FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key 
-AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.daily_low_temperature >= 17
-GROUP BY (M.subregion, M.parks, M.transit_stations, W.daily_high_temperature)
-
-/*Combined OLAP Operations Query (Outcomes and Weather Conditions for Cold Days with Precipitation)*/
-SELECT M.subregion, W.daily_low_temperature, W.precipitation, SUM(F.resolved::INT) AS total_resolved_cases, 
-SUM(F.unresolved::INT) AS total_unresolved_cases, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, weather_dimension as W
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key
-AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.daily_low_temperature <= 4 AND W.precipitation >= 0
-GROUP BY (M.subregion, W.daily_low_temperature, W.precipitation)
-
-/*Combined OLAP Operations Query (Mobility Levels Contrast Between Ottawa and Toronto)*/
-SELECT D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential, COUNT(*) AS total_cases
-FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, onset_date_dimension AS D
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.onset_date_surrogate_key = D.date_surrogate_key AND
-M.subregion in ('Ottawa Division', 'Toronto Division')
-GROUP BY (D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential)
-ORDER BY D.full_date
-
-/*Combined OLAP Operations Query (Contrast Weather Conditions and Total Cases)*/
-SELECT M.subregion, S.title, W.daily_high_temperature, W.daily_low_temperature, W.precipitation, AVG(F.total_cases) AS average_total_cases
-FROM (
- SELECT onset_date_surrogate_key, mobility_surrogate_key, weather_surrogate_key, special_measures_surrogate_key, COUNT(*) as total_cases 
- FROM covid19_tracking_fact_table 
- GROUP BY (onset_date_surrogate_key, mobility_surrogate_key, weather_surrogate_key, special_measures_surrogate_key)
-) AS F, mobility_dimension AS M, weather_dimension as W, special_measures_dimension as S
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.weather_surrogate_key = W.weather_surrogate_key AND F.special_measures_surrogate_key = S.special_measures_surrogate_key 
-AND M.subregion in ('Ottawa Division', 'Toronto Division') AND W.daily_low_temperature >= 17 AND W.precipitation <= 2.5 AND S.title = 'Stage 3'
-GROUP BY (M.subregion, S.title, W.daily_high_temperature, W.daily_low_temperature, W.precipitation)
-
-/*Iceberg Query (Top-N for Total Resolved Cases on a Date)*/
-SELECT D.full_date, SUM(F.resolved::INT) AS total_resolved_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key
-GROUP BY D.full_date
-ORDER BY total_resolved_cases DESC
-LIMIT 5
-
-/*Iceberg Query (Top-N for Total Fatal Cases on a Date)*/
-SELECT D.full_date, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key
-GROUP BY D.full_date
-ORDER BY total_fatal_cases DESC
-LIMIT 5
-
-/*Iceberg Query (Top-N for Total Cases on a Date)*/
-SELECT D.full_date, COUNT(*) AS total_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key
-GROUP BY D.full_date
-ORDER BY total_cases DESC
-LIMIT 5
-
-/*Iceberg Query (Bottom-N for Total Resolved Cases on a Date)*/
-SELECT D.full_date, SUM(F.resolved::INT) AS total_resolved_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key
-GROUP BY D.full_date
-ORDER BY total_resolved_cases ASC
-LIMIT 5
-
-/*Iceberg Query (Bottom-N for Total Resolved Cases on a Date)*/
-SELECT D.full_date, SUM(F.fatal::INT) AS total_fatal_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key
-GROUP BY D.full_date
-ORDER BY total_fatal_cases ASC
-LIMIT 5
-
-/* Le's Queries */
-
-/* 1-a-ii */
-SELECT D.Day, D.Week_in_Year, D.Month, D.Year, SUM(F.resolved::INT) AS total_resolve_cases
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key AND week_in_year = 41
-GROUP BY (D.year, D.month, D.Week_in_Year, D.day)
-ORDER BY D.year, D.month, D.Week_in_Year, D.day
-
-/* 1-b-i */
-SELECT L.phu_name, SUM(F.resolved ::INT) AS total_cases_resolved, SUM(F.unresolved ::INT) AS total_cases_unresolved, SUM(F.fatal ::INT) AS total_cases_fatal, COUNT(P.patient_surrogate_key) AS total_cases
-FROM covid19_tracking_fact_table AS F 
-INNER JOIN patient_dimension P ON F.patient_surrogate_key = P.patient_surrogate_key
-INNER JOIN phu_location_dimension L ON F.phu_location_surrogate_key = L.phu_location_surrogate_key
-WHERE phu_name = 'York Region Public Health Services'
-GROUP BY L.phu_name
-
-/* 1-d-iv */
-SELECT D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential, COUNT(*) AS total_cases
-FROM covid19_tracking_fact_table AS F, mobility_dimension AS M, onset_date_dimension AS D
-WHERE F.mobility_surrogate_key = M.mobility_surrogate_key AND F.onset_date_surrogate_key = D.date_surrogate_key AND
-M.subregion in ('Ottawa Division', 'Toronto Division')
-GROUP BY (D.full_date, M.subregion, M.retail_and_recreation, M.parks, M.transit_stations, M.workplaces, M.residential)
-ORDER BY D.full_date
-
-/* 2-b-iv */
-SELECT D.month, D.week_in_year, SUM(F.resolved ::INT) AS total_cases_resolved, RANK() OVER (PARTITION BY D.month ORDER BY SUM(F.resolved ::INT) DESC)
-FROM covid19_tracking_fact_table AS F, onset_date_dimension AS D
-WHERE F.onset_date_surrogate_key = D.date_surrogate_key
-GROUP BY (D.month, D.week_in_year)
-
-/* acquisition & age group vs total cases*/
-Select p.acquisition_group, p.age_group, COUNT(*) as total_cases, 
-RANK () OVER ( 
-		PARTITION BY p.age_group
-		ORDER BY COUNT(*) DESC
-	) as rank
-FROM
-covid19_tracking_fact_table f, 
-patient_dimension p 
-WHERE
-f.patient_surrogate_key = p.patient_surrogate_key
-AND acquisition_group != 'MISSING INFORMATION'
-AND age_group != 'UNKNOWN'
-GROUP BY
-(p.age_group, p.acquisition_group)
